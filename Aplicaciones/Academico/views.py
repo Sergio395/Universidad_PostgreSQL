a843b573be5f88db6ebc319a4418a3b0a427ba81
Sergio395 <tolser2019@gmail.com>
2023-05-27 16:47:23 -0300

    views-filtros-ordenamiento

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -5,6 +5,14 @@ from .models import Curso
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>CRUD en PostgreSQL</h1>')
-    cursosListados=Curso.objects.all()
+    
+    # cursosListados=Curso.objects.all()
+    # cursosListados=Curso.objects.all()[:5]
+    # cursosListados=Curso.objects.all()[4:9]
+    # cursosListados=Curso.objects.all().order_by('creditos','-nombre') #desc
+    #cursosListados=Curso.objects.filter(nombre='Historia')
+    #cursosListados=Curso.objects.filter(nombre__startswith='Q')
+    cursosListados=Curso.objects.filter(nombre__contains='g')
+    #cursosListados=Curso.objects.filter(creditos__gte=4) #__lte=4 es <=4
     return render(request,'gestionCursos.html',{"cursos": cursosListados})
     
