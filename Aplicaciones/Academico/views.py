cb502ec571ef3c4318fe24891616331084ceac40
Sergio395 <tolser2019@gmail.com>
2023-05-27 15:22:59 -0300

    gestionCursos-for

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from .models import Curso
 
 # Create your views here.
+def home(request):
+    #return HttpResponse('<h1>CRUD en PostgreSQL</h1>')
+    cursosListados=Curso.objects.all()
+    return render(request,'gestionCursos.html',{"cursos": cursosListados})
+    
