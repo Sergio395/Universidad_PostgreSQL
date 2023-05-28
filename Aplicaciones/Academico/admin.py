5dbc0ab37b5a25a343a919cd20ecef9d6e9febcb
Sergio395 <tolser2019@gmail.com>
2023-05-28 06:16:04 -0300

    models class docente Foreingkey

--- Aplicaciones/Academico/admin.py
+++ Aplicaciones/Academico/admin.py
@@ -8,12 +8,31 @@ class CursoAdmin(admin.ModelAdmin): #2 y #3
     
     #ordering=('-nombre',) #tupla indicarlo con la coma
     #ordering=('creditos', 'nombre')
-    search_fields=('nombre', 'creditos')
-    #list_display=('id','nombre','creditos') #+columnas,sobrescribe __str__ de model
+    #search_fields=('nombre', 'creditos')
+     #list_display=('id','nombre','creditos') #+columnas,sobrescribe __str__ de model
     #list_editable = ('nombre','creditos') #lista y edita(va con list_display)
     #list_display_links = ('nombre',)
     #list_filter =('creditos',)
     #list_per_page=3 #3 registros por pagina
-    #exclude = ('creditos',) #desaparecen 
+    #exclude = ('creditos',) #desaparecen
+    '''
+    fieldsets = (
+        (None,{
+            'fields':('nombre',)
+        }),
+        ('Advanced options',{
+            'classes':('collapse', 'wide', 'extrapretty'),
+            'fields': ('creditos',)
+        })
+        
+        
+    ) 
+    '''
     
 #2 admin.site.register(Curso, CursoAdmin)
+    list_display=('id','coloreado','creditos') # cambio nombre x datos, luego datos x
+    def datos(self,obj):
+        return obj.nombre.upper()
+    datos.short_description="CURSO (MAYUS)"
+    datos.empty_value_display = "???"
+    datos.admin_order_field = 'nombre'
--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.utils.html import format_html
 
+from .choices import sexos
 # Create your models here.
+class Docente(models.Model):
+    apellido_paterno=models.CharField(max_length=20,verbose_name='Apellido Paterno')
+    apellido_materno=models.CharField(max_length=20,verbose_name='Apellido Materno')
+    nombres=models.CharField(max_length=20,verbose_name='Nombre')
+    fecha_nacimiento=models.DateField(verbose_name='Fecha de Nacimiento')
+    sexo=models.CharField(max_length=1,choices=sexos,default='F')
+    
+    def nombre_completo(self):
+        return "{} {}, {}".format(self.apellido_paterno,self.apellido_materno,self.nombres)
+
+    def __str__(self):
+        return self.nombre_completo()
+
+
+
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=30)
@@ -9,3 +26,9 @@ class Curso(models.Model):
     def __str__(self):
         texto="{0} ({1})"
         return texto.format(self.nombre, self.creditos)
+    
+    def coloreado(self):
+        if self.creditos >= 4:
+            return format_html('<span style="color: blue;">{0}</span>'.format(self.nombre))
+        else:
+            return format_html('<span style="color: red;">{0}</span>'.format(self.nombre))
